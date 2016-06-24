7cac8fc99af23af29179f721a7d7e38ac015e5c1
Vladislav <Kasp.KasperLab@yandex.ru>
2016-06-24 22:56:20 +0300

    Update fast-farm-card.js

--- fast-farm-card.js
+++ fast-farm-card.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Fast Farm Card
 // @namespace    https://github.com/Kasp42/SteamSummer2016
-// @version      1.0
+// @version      1.1
 // @description  Fast Farm Card From Steam Summer 2016
 // @author       Kasper (Telegram: https://telegram.me/kasp42)
 // @match        http://store.steampowered.com/explore*
@@ -26,8 +26,8 @@ var ViewList = function(queueNumber){
         for(var i = 0; i < data.queue.length; i++)
             requests.push( jQuery.post( 'http://store.steampowered.com/app/'+data.queue[i], { appid_to_clear_from_queue: data.queue[i], sessionid: g_sessionID } ) );
         jQuery.when.apply(jQuery, requests).done(function(){
-            if( queueNumber < 3 ) ViewList( queueNumber );
+            if(queueNumber < 3) ViewList(queueNumber);
             else $J('#AutoFarmCard span').text('Done!');
-        });
-    });
+        }).fail(function(){ViewList(queueNumber)});
+    }).fail(function(){ViewList(queueNumber)});;
 };
